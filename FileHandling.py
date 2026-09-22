3fbfedec53a8ca31425988b171e6dffd24137aa0
Sruthi <49494161+sruthiValpireddy@users.noreply.github.com>
2026-09-22 13:17:02 -0400

    Add files via upload

---
+++ FileHandling.py
@@ -0,0 +1,24 @@
+import os
+#os.makedirs("/Users/sru/Desktop/python/sruthi-python", exist_ok=True)
+# with open("/Users/sru/Desktop/python/sruthi-python/filecontent1.txt","x") as fileopen1:
+#fileopen1.write("My grandpa is V.P.Jaya Chandra Reddy\n")
+
+with open("filecontent1.txt","r") as fileopen1:
+
+    cpnt= fileopen1.read()
+
+    print(cpnt)
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
